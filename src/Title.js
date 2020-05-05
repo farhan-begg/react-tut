8bb9a023d1ef307fe13bf127c01d23d1d7e7cd21
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-05 12:19:34 -0700

    title component

--- src/Title.js
+++ src/Title.js
@@ -1,11 +1,19 @@
 // src/Title.js
 
 import React from 'react'
+import './Title.css';
+
 
 function Title() {
     return (
-        <div>
-            <h1>SFPOPOS</h1>
+        <div className="Title">
+            <header>
+                <h1>SFPOPOS</h1>
+                <div className="Title-Subtitle">
+                    <h1>"San Francisco Privately Owned Public Open Spaces".</h1>
+                </div>
+
+            </header>
         </div>
     )
 }
