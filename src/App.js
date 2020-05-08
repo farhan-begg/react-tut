f968729e647f2d618b57ba7f6aec14c67d0035b7
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-08 13:00:57 -0700

    page 3

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
+import POPOSList from './POPOSList';
+
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
-
 export default App;
---
+++ src/POPOSList.js
@@ -0,0 +1,19 @@
+// src/POPOSList.js
+
+import React from 'react';
+
+import POPOSSpace from './POPOSSpace';
+
+
+function POPOSList() {
+    return (
+        <div>
+            <POPOSSpace />
+            <POPOSSpace />
+            <POPOSSpace />
+            <POPOSSpace />
+        </div>
+    )
+}
+
+export default POPOSList
