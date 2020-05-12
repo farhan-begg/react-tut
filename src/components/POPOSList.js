bd33ee7d0baf4dd662f40328146f88cd48d9162f
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-11 17:50:36 -0700

    finished react tutorial

--- src/components/POPOSList.js
+++ src/components/POPOSList.js
@@ -3,18 +3,18 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
-import data from './sfpopos-data.json'
-
+import data from '../sfpopos-data.json'
 function POPOSList() {
 
-    const spaces = data.map(({ title, address, images, hours }) => {
-
+    const spaces = data.map(({ title, address, images, hours }, i) => {
         return (
             <POPOSSpace
+                id={i}
+                key={title}
                 name={title}
                 address={address}
                 image={images[0]}
-                hours={hours} // add a new prop for hours here
+                hours={hours}
             />
         )
     })
